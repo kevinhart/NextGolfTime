357cb3d9e600df81f11f0d1919e3714928a5539a
bweldy1 <bweldy1@gmail.com>
2014-04-18 23:20:37 -0400

    work on creating new group

--- js/GroupControllers.js
+++ js/GroupControllers.js
@@ -1,16 +1,19 @@
 angular.module('myApp').controller('NewGroupCtrl', function ($scope, $state, $timeout, User, ClientService) {
+    
 
-    console.log("newgroupctrl");
 
-
-    $scope.checkAvailability = function(){
+    $scope.createGroup = function(isValid){
         //go look in the DB and see if its available
          $scope.searching = true;
 
         $timeout(function(){
             $scope.searching = false;
+                    if(isValid){
+            alert("TBD - INSERT INTO DB");
+        }
         }, 3000);
+        
+
     }
-    
     
 });
